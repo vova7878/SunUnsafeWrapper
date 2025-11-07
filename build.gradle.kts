ad6c543e0f3d135d9c35f54403bc09dd9ed58532
Vladimir Kozelkov <46866705+vova7878@users.noreply.github.com>
2025-11-07 15:01:06 +0700

    Set up publications to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,8 @@ android {
     }
 
     publishing {
-        singleVariant("release") {
+        multipleVariants {
+            allVariants()
             withSourcesJar()
         }
     }
@@ -28,16 +29,42 @@ dependencies {
     implementation(libs.r8.annotations)
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = false)
+    signAllPublications()
 
-            afterEvaluate {
-                from(components["release"])
+    coordinates(
+        groupId = "io.github.vova7878",
+        artifactId = "SunUnsafeWrapper",
+        version = project.version.toString()
+    )
+
+    pom {
+        name.set("SunUnsafeWrapper")
+        description.set("Wrapper over sun.misc.Unsafe for android")
+        inceptionYear.set("2025")
+        url.set("https://github.com/vova7878/SunUnsafeWrapper")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/license/mit")
+                distribution.set("repository")
             }
+        }
+
+        developers {
+            developer {
+                id.set("vova7878")
+                name.set("Vladimir Kozelkov")
+                url.set("https://github.com/vova7878")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/vova7878/SunUnsafeWrapper")
+            connection.set("scm:git:git://github.com/vova7878/SunUnsafeWrapper.git")
+            developerConnection.set("scm:git:ssh://git@github.com/vova7878/SunUnsafeWrapper.git")
         }
     }
 }
